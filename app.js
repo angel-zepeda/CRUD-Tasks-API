3e89cca5314c12e34a2958e10eb88d63eb1e7ff0
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-07-31 11:25:30 -0500

    Add mlab mongo

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var cors = require('cors')
 
 var taksRoutes = require('./routes/task');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
