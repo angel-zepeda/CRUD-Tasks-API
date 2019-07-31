4e087800d7ff7d933cad571edf4059339b63622f
angel-zepeda <angel.zepeda0508@hotmail.com>
2019-07-31 12:22:12 -0500

    Add BD env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config({ path: 'variables.env' })
 
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
-const DB = 'mongodb://nodetaskapi:rodneymullen1@ds157641.mlab.com:57641/heroku_2sqrv2d6';
+const DB = process.env.DB || 'mongodb://nodetaskapi:rodneymullen1@ds157641.mlab.com:57641/heroku_2sqrv2d6';
 
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
